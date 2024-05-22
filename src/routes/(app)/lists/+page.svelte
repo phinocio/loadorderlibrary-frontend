1bf6b52544e32c60622579a685df4e88820e785c
Phinocio <github@phinocio.com>
2024-05-21 23:17:35 -0600

    feat: create search feature for browse page

--- src/routes/(app)/lists/+page.svelte
+++ src/routes/(app)/lists/+page.svelte
@@ -7,8 +7,11 @@
 	import { goto } from '$app/navigation';
 	import SortAscIcon from '$lib/components/icons/SortAsc.svelte';
 	import HashStraightIcon from '$lib/components/icons/HashStraight.svelte';
+	import SearchIcon from '$lib/components/icons/Search.svelte';
 
 	export let data: PageData;
+	let searchText: string = $page.url.searchParams.get('query') ?? '';
+	let listsPerPage: string = $page.url.searchParams.get('page[size]') ?? '30';
 
 	$: ({ author, game, lists } = data);
 	$: params = $page.url.searchParams;
@@ -19,9 +22,19 @@
 		goto(`/lists?${urlParams.toString()}`, { invalidateAll: true });
 	}
 
-	function perPage(num: string) {
+	function perPage() {
 		let urlParams = $page.url.searchParams;
-		urlParams.set('page[size]', num);
+		urlParams.set('page[size]', listsPerPage);
+		goto(`/lists?${urlParams.toString()}`, { invalidateAll: true });
+	}
+
+	function searchLists() {
+		let urlParams = $page.url.searchParams;
+		if (searchText) {
+			urlParams.set('query', searchText);
+		} else {
+			urlParams.delete('query');
+		}
 		goto(`/lists?${urlParams.toString()}`, { invalidateAll: true });
 	}
 </script>
@@ -46,7 +59,7 @@
 	<meta name="twitter:image" content="/images/logo.png" />
 </svelte:head>
 
-<div class="mb-4 flex flex-col items-center justify-between md:flex-row">
+<div class="mb-6 flex flex-col items-center justify-between md:flex-row">
 	<h1 class="text-3xl font-bold">
 		{game ?? 'All'} Lists {#if author}
 			by <a
@@ -73,17 +86,15 @@
 				name="per-page"
 				id="per-page"
 				class="appearance-none rounded-r-full bg-gray-200 pr-4 text-right dark:bg-[#26263a]"
-				on:change={({ target }) => perPage(target?.value)}
+				bind:value={listsPerPage}
+				on:change={() => perPage()}
 				aria-label="Lists Per Page"
 			>
-				<option value="5" selected={browser && params.get('page[size]') === '5'}>5</option>
-				<option value="15" selected={browser && params.get('page[size]') === '15'}>15</option>
-				<option
-					value="30"
-					selected={(browser && params.get('page[size]') === '30') || !params.get('page[size]')}>30</option
-				>
-				<option value="60" selected={browser && params.get('page[size]') === '60'}>60</option>
-				<option value="90" selected={browser && params.get('page[size]') === '90'}>90</option>
+				<option value="5" selected={listsPerPage === '5'}>5</option>
+				<option value="15" selected={listsPerPage === '15'}>15</option>
+				<option value="30" selected={listsPerPage === '30'}>30</option>
+				<option value="60" selected={listsPerPage === '60'}>60</option>
+				<option value="90" selected={listsPerPage === '90'}>90</option>
 			</select>
 		</section>
 
@@ -111,13 +122,39 @@
 		</section>
 	</div>
 </div>
-<section class="grid grid-cols-1 gap-10 md:grid-cols-3 md:gap-10">
+<div class="mb-6">
+	<form class="flex w-full min-w-full">
+		<label for="name" class="relative block w-full">
+			<SearchIcon class="pointer-events-none absolute left-4 top-1/2 h-6 w-6 -translate-y-1/2 transform" />
+
+			<input
+				type="text"
+				name="name"
+				id="name"
+				bind:value={searchText}
+				class="flex w-full min-w-full rounded-l-xl bg-gray-200 p-4 pl-14 focus:outline-none focus:ring-1 focus:ring-blue-500 dark:bg-[#26263a] md:w-auto"
+				placeholder="Filter..."
+				autocomplete="off"
+				required
+			/>
+		</label>
+		<button
+			class="rounded-r-xl bg-blue-500 px-8 py-4 text-white hover:bg-blue-600"
+			on:click|preventDefault={() => searchLists()}
+		>
+			<SearchIcon class="pointer-events-none h-6 w-6" />
+		</button>
+	</form>
+
+	<span>Searching will look for matches in a list Name, Game, Description, or Author.</span>
+</div>
+<section class="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-10 lg:grid-cols-3">
 	{#if lists.data.length > 0}
 		{#each lists.data as list}
 			<BrowseList {list} />
 		{/each}
 	{:else}
-		<p>
+		<p class="md:col-spane-2 lg:col-span-3">
 			No lists available to show, why not <a
 				href="/upload"
 				class="text-blue-600 hover:text-blue-400 active:text-blue-400 dark:text-blue-400 dark:hover:text-blue-600 dark:active:text-blue-600"
