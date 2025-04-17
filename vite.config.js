826043e8d438adad5bb77d8fb2f1a4880b89ea41
Phinocio <github@phinocio.com>
2025-04-16 19:50:26 -0600

    chore: config for https url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['testing.lol.wonderland'],
+		allowedHosts: ['lol.wonderland.phinoc.io'],
 	},
 });
