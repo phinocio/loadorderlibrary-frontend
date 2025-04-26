89ce1dced16267dda4a9067151ef770de80c641f
Phinocio <github@phinocio.com>
2025-04-26 15:11:15 -0600

    refactor: extract get user to function in query hook

--- src/hooks/queries/use-admin-user.ts
+++ src/hooks/queries/use-admin-user.ts
@@ -1,9 +1,16 @@
-import { adminGetUsers } from "@/api/admin-user";
+import { adminGetUser, adminGetUsers } from "@/api/admin-user";
 
 export const adminUsersListQueryOptions = {
 	queryKey: ["admin-users"],
 	queryFn: adminGetUsers,
 };
+
+export function adminUserListQueryOptions(name: string) {
+	return {
+		queryKey: ["admin-users", name],
+		queryFn: () => adminGetUser(name),
+	};
+}
 
 export function useAdminUser() {
 	// const queryClient = useQueryClient();
--- src/routes/admin/users/$name.tsx
+++ src/routes/admin/users/$name.tsx
@@ -1,23 +1,19 @@
-import { adminGetUser } from "@/api/admin-user";
+import { adminUserListQueryOptions } from "@/hooks/queries/use-admin-user";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/admin/users/$name")({
 	loader: async ({ context, params }) => {
-		await context.queryClient.ensureQueryData({
-			queryKey: ["admin-users", params.name],
-			queryFn: () => adminGetUser(params.name),
-		});
+		await context.queryClient.ensureQueryData(
+			adminUserListQueryOptions(params.name),
+		);
 	},
 	component: UserComponent,
 });
 
 function UserComponent() {
 	const { name } = Route.useParams();
-	const { data: user } = useSuspenseQuery({
-		queryKey: ["admin-users", name],
-		queryFn: () => adminGetUser(name),
-	});
+	const { data: user } = useSuspenseQuery(adminUserListQueryOptions(name));
 
 	return <div>Hello {user?.name}</div>;
 }
